2b8db8cb7f9b3284f01a159c2af62c52aabf027a
TurovaE <ekaterina.turova@mail.ru>
2023-05-30 23:42:38 +0700

    create card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Card from './components/Card';
+
+const card1 = {
+  title: 'Card title',
+  text: 'Some quick example text to build on the card title and macke up the bulk of the card\'s content',
+}
+
+const card2 = {
+  title: 'Special title treatment',
+  text: 'With supporting text below as a natural lead-in to additional content.',
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Card {...card1}>
+        <img src="https://img.freepik.com/free-photo/design-space-paper-textured-background_53876-147801.jpg?w=740&t=st=1685464680~exp=1685465280~hmac=e81b2bc3d2dd31889e1621985160e55c23d820f760ea132d98b9992683c488b1" alt="..." className='card-img-top' />
+      </Card>
+      <Card {...card2}/>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(( {command} ) => {
-  console.log(command);
 
   const setting = {
     plugins: [react()],
